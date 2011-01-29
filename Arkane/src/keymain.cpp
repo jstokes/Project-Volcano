0748f535aac9f6cb8481cd451d733663a064f095
Jeff Stokes <stokesj07@students.ecu.edu>
2011-01-29 00:25:04 -0500

    heh..multi-threaded electronic keyboard (now with 7 keys added) *woot*

--- Arkane/src/keymain.cpp
+++ Arkane/src/keymain.cpp
@@ -2,113 +2,144 @@
 
 #include <Windows.h>
 #include <stdio.h>
-#include <fmod.h>
+#include <fmod.hpp>
 #include <fmod_errors.h>
 #include <conio.h>
 #include <math.h>
+#include <process.h>
 
-//globals
 const int NUM_CHANNELS = 7;
-FMOD_SYSTEM      *system;
-FMOD_CHANNEL     *channels[NUM_CHANNELS];
-FMOD_DSP         *dsphighpass = 0;
+FMOD::Channel *channel[NUM_CHANNELS];
 
-FMOD_RESULT       result;
-int               key = 0;
-unsigned int      version;
+
+struct thread_args {
+	FMOD::System      *system;
+	FMOD::DSP         *dsp;
+	int               threadNo;
+	bool              paused;
+};
 
 void ERRCHECK(FMOD_RESULT result)
 {
     if (result != FMOD_OK)
     {
-        //printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
+        printf("FMOD error! (%d) %s\n", result, FMOD_ErrorString(result));
         exit(-1);
     }
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam){
+void startKey(void* args) {
 
-	switch(message){
+	thread_args* t = (thread_args*)args;
 
-		case WM_CREATE:
-			return 0;
-			break;
+	int key = 0;
+	FMOD::System   *system  = t->system;
+	FMOD::DSP      *dsp     = t->dsp;
+	int threadNo            = t->threadNo;
+	bool paused             = t->paused;
+	FMOD_RESULT result;
+	bool current_status;
 
-		case WM_CLOSE:
-			PostQuitMessage(0);
-			return 0;
-			break;
+	channel[threadNo]->getPaused(&current_status);
 
-		case WM_PAINT:
-			return 0;
-			break;
+	// if no change needs to be made
+	if (current_status == paused) _endthread();
 
-		case WM_KEYDOWN:
-			switch(wParam){
-				case '1':
-
-					return 0;
-					break;
-				default:
-					break;
-			}
-			break;
-		
-		case WM_KEYUP:
-
-		default:
-			break;
-	}
-	return (DefWindowProc(hwnd, message, wParam, lParam));
+	result = system->playDSP(FMOD_CHANNEL_REUSE, dsp, true, &channel[threadNo]);
+	channel[threadNo]->setPaused(paused);
+	_endthread();
 }
 
-void initializeChannels() {
+int main(int argc, char* argv[]) {
+	
+	for (int i = 0; i < 80; i++) printf("-");
+	printf("\n");
+	for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 30; i++) printf("-");
+		for (int i = 0; i < 20; i++) printf(" ");
+		for (int i = 0; i < 30; i++) printf("-");
+		printf("\n");
+	}
+
+	printf("\t\tArkane Studios Presents:\n");
+	printf("\t\t\tA Team Jeff Production\n");
+	printf("\t\t\t\tUltimate keyboard 3000\n");
+
+	for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 30; i++) printf("-");
+		for (int i = 0; i < 20; i++) printf(" ");
+		for (int i = 0; i < 30; i++) printf("-");
+		printf("\n");
+	}
+	for (int i = 0; i < 80; i++) printf("-");
+
+	printf("To use press key numbers 1-7.  They each represent a different key on \nthe electronic keyboard\n\n");
+
+	/*
+		Initialize variables for program
+	*/
+	FMOD::System   *system;
+	FMOD_RESULT     result;
+	int             key = 0;
+	FMOD::DSP      *dsp[NUM_CHANNELS];
+
+    // Create system and initialize
+	result = FMOD::System_Create(&system); ERRCHECK(result);
+	result = system->init(NUM_CHANNELS, FMOD_INIT_NORMAL, NULL); ERRCHECK(result);
+
+	// Set the frequencies of all the DSPs
+	// Based on piano key frequencies
 	const float base_freq = 440.0f;
 	float freq = base_freq;
 	for (int i = 0; i < NUM_CHANNELS; i++) {
-		channels[i] = 0;
-		FMOD_Channel_SetFrequency(channels[i], freq);
-		freq = base_freq * pow(2.0, (i/12)); // calculate next step up
+		
+		result = system->createDSPByType(FMOD_DSP_TYPE_OSCILLATOR, &dsp[i]);
+		ERRCHECK(result);
+
+		result = dsp[i]->setParameter(FMOD_DSP_OSCILLATOR_RATE, freq);
+		ERRCHECK(result);
+		freq = base_freq * pow(2.0f, ((i + 1)/12.0f)); // calculate next step up
 	}
-}
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd){
+	thread_args t[NUM_CHANNELS];
+	// Create thread arguments for each key
+	for (int i = 0; i < NUM_CHANNELS; i++) {
+		/* Args for threads! */
+		t[i].threadNo = i; // thread number
+		t[i].system   = system;
+		t[i].dsp      = dsp[i];
+	}
 
-	WNDCLASSEX windowClass;
-	HWND       hwnd;
-	MSG        msg;
+	int begin = 49; // Beginning key is the '1' key, for now
+	do {
+		for (int i = 0; i < NUM_CHANNELS; i++) { // check keys from 0-num_channels
+			if (GetKeyState(begin + i) & 0x80) { // key is pressed
+				t[i].paused = false;
+			} else t[i].paused = true;           // key is not pressed
 
-	windowClass.cbSize        = sizeof(WNDCLASSEX);
-	windowClass.style         = CS_HREDRAW | CS_VREDRAW;
-	windowClass.lpfnWndProc   = WndProc;
-	windowClass.cbClsExtra    = 0;
-	windowClass.cbWndExtra    = 0;
-	windowClass.hInstance     = hInstance;
-	windowClass.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
-	windowClass.hCursor       = LoadCursor(NULL, IDC_ARROW);
-	windowClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	windowClass.lpszMenuName  = NULL;
-	windowClass.lpszClassName = L"MyClass";
-	windowClass.hIconSm       = LoadIcon(NULL, IDI_WINLOGO);
+			_beginthread(startKey, 0, (void*)&t[i]);
+		}
+		result = system->update();
 
-	if(!RegisterClassEx(&windowClass))
-		return 0;
 
-	hwnd = CreateWindowEx(NULL,
-						  L"MyClass",
-						  L"VolcanoStudio",
-						  WS_OVERLAPPEDWINDOW | WS_VISIBLE | WS_SYSMENU | WS_CAPTION,
-						  100, 100,
-						  400, 400,
-						  NULL,
-						  NULL,
-						  hInstance,
-						  NULL);
-	if(!hwnd)
-		return 0;
+		char* channels_playing[NUM_CHANNELS];
 
-	result = FMOD_System_Create(&system); ERRCHECK(result);
-	result = FMOD_System_Init(system, NUM_CHANNELS, FMOD_INIT_NORMAL, NULL); ERRCHECK(result);
+		for (int i = 0; i < NUM_CHANNELS; i++) {
+			if (!t[i].paused) {
+				channels_playing[i] = (char*)2;
+			} else channels_playing[i] = " ";
+		}
+			
+		printf("Channels playing: ");
+		for (int i = 0; i < NUM_CHANNELS; i++) {
+			printf("%c", channels_playing[i]);
+			if (i != NUM_CHANNELS - 1) printf(" ");
+		}
+		printf("\r");
+		
+		Sleep(10);
+	
+	} while(true);
 
-	initializeChannels();
+	return 0;
 }
